cd67acbf5750b6059de847c089c2b9f2c8b1b17f
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2025-02-24 12:04:41 +0530

    changes

---
+++ JavaScript/Problems/ArrowFun.js
@@ -0,0 +1,8 @@
+let areaRect = (a, b)=>{
+    console.log("Area = " + a*b);
+}
+
+var height = 6;
+var width = 12;
+
+areaRect(height, width);
---
+++ JavaScript/Problems/Functions.js
@@ -0,0 +1,8 @@
+function add(x, y){
+    console.log(x+y);
+}
+
+var a = 5;
+var b = 5;
+
+add(a, b);
